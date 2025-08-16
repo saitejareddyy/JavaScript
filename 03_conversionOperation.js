95a6087f2530953de6f8491efbb3c82fa877168d
saitejareddyy <saitejareddy0502@gmail.com>
2025-08-16 14:45:33 +0530

    conversions

---
+++ 03_conversionOperation.js
@@ -0,0 +1,15 @@
+let score = "33abc"
+
+let value = null
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(typeof score)
+console.log(valueInNumber)
+
+
+
+console.log(typeof value)
+let new_value = Number(value)
+console.log(new_value)
+console.log(typeof new_value);
